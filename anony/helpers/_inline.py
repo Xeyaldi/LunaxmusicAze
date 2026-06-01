758411dee971277aca7262ea6bca32a22c0691dc
Xeyaldi <167771989+Xeyaldi@users.noreply.github.com>
2026-06-01 23:42:23 +0400

    Update _inline.py
    Signed-off-by: Xeyaldi <167771989+Xeyaldi@users.noreply.github.com>

--- anony/helpers/_inline.py
+++ anony/helpers/_inline.py
@@ -153,7 +153,7 @@ class Inline:
                 [
                     self.ikb(
                         text=lang["source"],
-                        url="https://t.me/faristylee",
+                        url="https://t.me/lunadidaa",
                     )
                 ]
             ]
