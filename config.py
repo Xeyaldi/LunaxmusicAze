55427e56eabb34247c4cf9e5a6692a3e32ca3ba9
Xeyaldi <167771989+Xeyaldi@users.noreply.github.com>
2026-06-01 23:40:30 +0400

    Update configuration limits and support channel URL
    Signed-off-by: Xeyaldi <167771989+Xeyaldi@users.noreply.github.com>

--- config.py
+++ config.py
@@ -14,15 +14,15 @@ class Config:
         self.LOGGER_ID = int(getenv("LOGGER_ID", 0))
         self.OWNER_ID = int(getenv("OWNER_ID", 0))
 
-        self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", 60)) * 60
-        self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", 20))
-        self.PLAYLIST_LIMIT = int(getenv("PLAYLIST_LIMIT", 20))
+        self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", 1500)) * 1500
+        self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", 200))
+        self.PLAYLIST_LIMIT = int(getenv("PLAYLIST_LIMIT", 200))
 
         self.SESSION1 = getenv("SESSION", None)
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/HT_bots")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/kepeneyim")
         self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me//HT_bots_chat")
 
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", "False").lower() == "true"
