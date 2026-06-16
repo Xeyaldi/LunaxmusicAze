42e2d240668cc41e76387b595c1182b8426cbdb5
Xeyaldi <167771989+Xeyaldi@users.noreply.github.com>
2026-06-16 22:00:06 +0400

    Update support channel and chat URLs in config.py
    Signed-off-by: Xeyaldi <167771989+Xeyaldi@users.noreply.github.com>

--- config.py
+++ config.py
@@ -22,8 +22,8 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/duygublog")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+WlKMIpP2Noo1NGJk")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ht_bots")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ht_bots_chaf")
 
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", "False").lower() == "true"
         self.AUTO_END: bool = getenv("AUTO_END", "False").lower() == "true"
